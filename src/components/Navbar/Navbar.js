861d0321a4fdd9df3317cafaf1e49184644e325e
Phillip <phillip.welch2@mavs.uta.edu>
2022-04-11 00:50:07 -0500

    navbar/mobile nav seem to be working

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,24 +8,38 @@ import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "./Navbar.css";
 
 export default function Navbar() {
+
+  const [navState, setNavState] = useState("false");
+  const [navButton, setNavButton] = useState(faBars);
+
+  const toggleNav = (event) => {
+    if (navState === "true") {
+      setNavState("false");
+      setNavButton(faBars);
+    } else {
+      setNavState("true");
+      setNavButton(faXmark);
+    }
+  };
+
   return (
     <div className="navbar-container">
       <nav className="navbar">
-        <button className="nav-button">
-          <FontAwesomeIcon icon={faBars} className="nav-button-icon" />
+        <button className="nav-button" onClick={toggleNav}>
+          <FontAwesomeIcon icon={navButton} className="nav-button-icon" />
         </button>
 
-        <ul className="nav-link-list mobile-nav" data-visible="true">
-          <Link to="/about" className="nav-link link-1">
+        <ul className="nav-link-list mobile-nav" data-visible={navState}>
+          <Link to="/about" className="nav-link link-1" onClick={toggleNav}>
             <li className="nav-list-item">About</li>
           </Link>
-          <Link to="/portfolio" className="nav-link link-2">
+          <Link to="/portfolio" className="nav-link link-2" onClick={toggleNav}>
             <li className="nav-list-item">Portfolio</li>
           </Link>
-          <Link to="/contact" className="nav-link link-3">
+          <Link to="/contact" className="nav-link link-3" onClick={toggleNav}>
             <li className="nav-list-item">Contact</li>
           </Link>
-          <Link to="/resume" className="nav-link link-4">
+          <Link to="/resume" className="nav-link link-4" onClick={toggleNav}>
             <li className="nav-list-item">Resumé</li>
           </Link>
         </ul>

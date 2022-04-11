b94422831494b0421b89fcb9d0e399224ba6ee54
Phillip <phillip.welch2@mavs.uta.edu>
2022-04-11 01:20:47 -0500

    moving on to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,31 +1,44 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+  faLinkedin,
+  faLinkedinIn,
+  faGithub,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 import logo from "../../logo.png";
 import "./Footer.css";
 
-
-
 export default function Footer() {
   return (
     <div className="footer">
-        <ul>
-            <li>
-                <i>LinkedIn</i>
-            </li>
-            <li>
-                <i>Github</i>
-            </li>
-            <li>
-                <i>Twitter</i>
-            </li>
-        </ul>
-        <h3>Copyright &copy; Phillip Welch 2022</h3>
+
+      <ul className="footer-link-list">
+
+        <Link to="" className="footer-link link-1">
+          <li className="nav-list-item">
+            <FontAwesomeIcon icon={faLinkedin} className="footer-icon" />
+          </li>
+        </Link>
+
+        <Link to="" className="footer-link link-1">
+          <li className="nav-list-item">
+            <FontAwesomeIcon icon={faGithub} className="footer-icon" />
+          </li>
+        </Link>
+
+        <Link to="" className="footer-link link-1">
+          <li className="nav-list-item">
+            <FontAwesomeIcon icon={faTwitter} className="footer-icon" />
+          </li>
+        </Link>
+      </ul>
+
+      <h3>Copyright &copy; Phillip Welch 2022</h3>
     </div>
   );
 }

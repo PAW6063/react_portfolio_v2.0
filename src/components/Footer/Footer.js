4d5bf94d9ece526ed44ae08273fe0c573c387eb9
Phillip <Phill@Ashleys-MacBook-Pro-2.local>
2022-04-12 18:46:08 -0500

    finished footer moving on to contact page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,6 @@ import {
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 
-import logo from "../../logo.png";
 import "./Footer.css";
 
 export default function Footer() {
@@ -20,25 +19,25 @@ export default function Footer() {
       <ul className="footer-link-list">
 
         <Link to="" className="footer-link link-1">
-          <li className="nav-list-item">
-            <FontAwesomeIcon icon={faLinkedin} className="footer-icon" />
+          <li className="footer-list-item">
+            <FontAwesomeIcon icon={faLinkedin} className="footer-icon icon-1" />
           </li>
         </Link>
 
-        <Link to="" className="footer-link link-1">
-          <li className="nav-list-item">
-            <FontAwesomeIcon icon={faGithub} className="footer-icon" />
+        <Link to="" className="footer-link link-2">
+          <li className="footer-list-item">
+            <FontAwesomeIcon icon={faGithub} className="footer-icon icon-2" />
           </li>
         </Link>
 
-        <Link to="" className="footer-link link-1">
-          <li className="nav-list-item">
-            <FontAwesomeIcon icon={faTwitter} className="footer-icon" />
+        <Link to="" className="footer-link link-3">
+          <li className="footer-list-item">
+            <FontAwesomeIcon icon={faTwitter} className="footer-icon icon-3" />
           </li>
         </Link>
       </ul>
 
-      <h3>Copyright &copy; Phillip Welch 2022</h3>
+      <h3 className="footer-copyright" >Copyright &copy; Phillip Welch 2022</h3>
     </div>
   );
 }
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,7 +6,6 @@ import {
   Link
 } from "react-router-dom";
 
-import logo from "../../logo.png";
 import "./Contact.css";
 
 
@@ -14,34 +13,34 @@ import "./Contact.css";
 export default function Contact() {
   return (
     <div className="contact">
-        <h1>{"<Contact Me/>"}</h1>
+        <h1 className="main-header contact-header">{"<Contact Me/>"}</h1>
 
-        <h3>LinkedIn</h3>
-        <ul>
-            <li>
-                <a></a>
+        <h3 className="info-header" >LinkedIn</h3>
+        <ul className="info-list" >
+            <li className="info-list-item" >
+                <a className="info-link" ></a>
             </li>
         </ul>
 
-        <h3>Email</h3>
-        <ul>
-            <li>
-                <a></a>
+        <h3 className="info-header" >Email</h3>
+        <ul className="info-list" >
+            <li className="info-list-item" >
+                <a className="info-link" ></a>
             </li>
         </ul>
         
-        <h2>Message Me!</h2>
-        <form>
-            <label>Name</label>
-            <input/>
+        <h2 className="contact-header">Message Me!</h2>
+        <form className="contact-form">
+            <label className="form-label name-label">Name</label>
+            <input className=" form-input name-input"/>
 
-            <label>Email</label>
-            <input/>
+            <label className="form-label email-label">Email</label>
+            <input className="form-input email-input"/>
 
-            <label>Message</label>
-            <input/>
+            <label className="form-label message-label">Message</label>
+            <input className="form-input message input"/>
 
-            <button></button>
+            <button className="form-button"></button>
         </form>
     </div>
   );

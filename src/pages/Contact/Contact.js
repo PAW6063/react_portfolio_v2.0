c3b48aa89952cba83fdbadd8f0b25ab4b5053a24
Phillip <phillip.welch2@mavs.uta.edu>
2022-04-13 15:06:52 -0500

    working on contact page still in progress

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,47 +1,38 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./Contact.css";
-
-
 
 export default function Contact() {
   return (
     <div className="contact">
-        <h1 className="main-header contact-header">{"<Contact Me/>"}</h1>
+      <h1 className="main-header contact-header">{"<Contact Me/>"}</h1>
 
-        <h3 className="info-header" >LinkedIn</h3>
-        <ul className="info-list" >
-            <li className="info-list-item" >
-                <a className="info-link" ></a>
-            </li>
-        </ul>
+      <ul className="info-list">
+        <h3 className="info-header">LinkedIn</h3>
+        <Link className="info-link">
+          <li className="info-list-item">phillip-welch-67a533214/</li>
+        </Link>
 
-        <h3 className="info-header" >Email</h3>
-        <ul className="info-list" >
-            <li className="info-list-item" >
-                <a className="info-link" ></a>
-            </li>
-        </ul>
-        
-        <h2 className="contact-header">Message Me!</h2>
-        <form className="contact-form">
-            <label className="form-label name-label">Name</label>
-            <input className=" form-input name-input"/>
+        <h3 className="info-header">Email</h3>
+        <Link className="info-link">
+          <li className="info-list-item">phillip.w3lch455@gmail.com</li>
+        </Link>
+      </ul>
 
-            <label className="form-label email-label">Email</label>
-            <input className="form-input email-input"/>
+      <h2 className="contact-header">Message Me!</h2>
+      <form className="contact-form">
+        <label className="form-label name-label">Name</label>
+        <input className=" form-input name-input" />
 
-            <label className="form-label message-label">Message</label>
-            <input className="form-input message input"/>
+        <label className="form-label email-label">Email</label>
+        <input className="form-input email-input" />
 
-            <button className="form-button"></button>
-        </form>
+        <label className="form-label message-label">Message</label>
+        <textarea className="form-input message-input" />
+
+        <button className="form-button">Send</button>
+      </form>
     </div>
   );
 }

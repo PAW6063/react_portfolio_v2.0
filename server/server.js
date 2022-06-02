22d98e459b7749961818a1335c7bb3b881514791
Phillip <phillip.welch2@mavs.uta.edu>
2022-06-01 19:01:51 -0500

    homepage in package.json

--- server/server.js
+++ server/server.js
@@ -73,15 +73,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.use("/", router);
+app.use(express.static(path.join(__dirname, '../client/build')), router);
 
 app.listen(PORT, () => {
   console.log(`Application listening on port ${PORT}...`);
